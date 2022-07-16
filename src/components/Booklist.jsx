49bcefdda9febd84d14ffc625aa4e2a9faa05721
akinatty <76603870+akinatada@users.noreply.github.com>
2022-07-16 11:44:29 +0900

    first commit

---
+++ src/components/Booklist.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const Booklist = ({ language, getData }) => {
+    const [bookData, setBookData] = useState(null);
+
+    useEffect(() => {
+        const result = getData?.(language).then((response) =>
+            setBookData(response)
+        );
+    }, [language, getData]);
+
+    return (
+        <ul>
+            {bookData === null ? (
+                <p>now loading...</p>
+            ) : (
+            bookData.data.items.slice(0, 5).map((x, index) => (
+                <li key={index}>{x.volumeInfo.title} / {x.volumeInfo.publishedDate} / {x.volumeInfo.authors} / <a href={x.volumeInfo.imageLinks.smallThumbnail}>書籍サムネイル</a> </li>
+            ))
+        )}
+        
+        </ul>
+    );
+};
+
